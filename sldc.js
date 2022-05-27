3a7290469797cf4c47af3473692c7b9e18594182
DanishCoder <Danishim66@gmail.com>
2022-05-27 12:03:32 -0400

    Part 1 complete

---
+++ sldc.js
@@ -0,0 +1,32 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of requirements
+// Design
+// Implementation
+// Testing and Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
+
+let step1 = "Planning"
+let step2 = "Analysis of requirements"
+let step3 = "Design"
+let step4 = "Implementation"
+let step5 = 'Testing & Integration'
+let step6 = "Maintain"
+
+console.log(`Step one of the Software Development Cycle is ${step1}`)
+console.log(`Step two of the Software Development Cycle is ${step2}`)
+console.log(`Step three of the Software Development Cycle is ${step3}`)
+console.log(`Step four of the Software Development Cycle is ${step4}`)
+console.log(`Step five of the Software Development Cycle is ${step5}`)
+console.log(`Step six of the Software Development Cycle is ${step6}`)
