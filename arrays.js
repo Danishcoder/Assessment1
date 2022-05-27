d6020037bd1fcf575fb5abcd452df9d8cc1e53bf
DanishCoder <Danishim66@gmail.com>
2022-05-27 13:50:06 -0400

    Array file completed

--- arrays.js
+++ arrays.js
@@ -5,10 +5,17 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+const colorCopy = faveColors.slice();
+console.log(colorCopy)
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+
+colorCopy.push('blue');
+console.log(colorCopy)
+
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -20,7 +27,8 @@ const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
 
-
+middleNums = numbers.splice(1,3)
+console.log(middleNums)
 //////////////////PROBLEM 4////////////////////
 /* 
   Create a function called 'bigOrSmall' that takes in one parameter, 'arr', 
@@ -35,3 +43,18 @@ const numbers = [1, 2, 3, 4, 5]
 */
 
 // CODE HERE
+
+function bigOrSmall(arr) {
+  const answers = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] <= 100) {
+      answers.push('small');
+    } else {
+      answers.push('big')
+    }
+    }
+    return answers
+   }
+   console.log(bigOrSmall([0, 105, 100]))
+
+   
